12ef5c9662888d24680102b6d63747c3f7898a9b
colevandersWands <evan.t.cole@gmail.com>
2017-09-30 21:25:11 +0200

    reorganized

--- js/network.js
+++ js/network.js
@@ -1,89 +1,97 @@
-// create an array with nodes
-var nodes = new vis.DataSet([
-    {id: 0, label: 'communications', title: 'I have a popup!'},
-    {id: 1, label: 'Node 1', title: 'I have a popup!'},
-    {id: 2, label: 'Node 2', title: 'I have a popup!'},
-    {id: 3, label: 'Node 3', title: 'I have a popup!'},
-    {id: 4, label: 'Node 4', title: 'I have a popup!'},
-    {id: 5, label: 'Node 5', title: 'I have a popup!'}
-]);
+window.onload = function() {
+    // create an array with nodes
+    var nodes = new vis.DataSet([
+        {id: 00, label: 'being a developer', title: 'devving is collaboration and communication'},
+        {id: 01, label: 'gh portfolio', title: 'your public face to the development world'},
+        {id: 02, label: 'cleancalc 0-2', title: 'separate logic and framework'},
+        {id: 03, label: 'be google', title: 'make a pretty page'},
+        {id: 04, label: 'tictactoes', title: 'learn MVC'},
+        {id: 05, label: 'battleship', title: 'feature-based development, from the front to the back'},
+        {id: 06, label: 'mvc-lh calc', title: 'tictactoes meets cleancalc'},
+        {id: 07, label: 'data model', title: 'think about your data, prepare for *book'},
+        {id: 08, label: 'mini group project', title: 'a mini group project'},
+        {id: 09, label: 'assessment', title: 'all the skills you\'ll need for node'},
+        {id: 10, label: '*Book', title: 'bringing it all together'},
+        {id: 11, label: 'be youtube', title: 'using node as a build environmnet, axios'}
+    ]);
 
-// create an array with edges
-var edges = new vis.DataSet([
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
-]);
+    // create an array with edges
+    var edges = new vis.DataSet([
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]);
 
-// create a network
-var container = document.getElementById('mynetwork');
-var data = {
-    nodes: nodes,
-    edges: edges
+    // create a network
+    var container = document.getElementById('mynetwork');
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {interaction:{hover:true}};
+    var network = new vis.Network(container, data, options);
+
+    network.on("click", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4);
+        console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    });
+    network.on("doubleClick", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>doubleClick event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("oncontext", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>oncontext (right click) event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("dragStart", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>dragStart event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("dragging", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>dragging event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("dragEnd", function (params) {
+        params.event = "[original event]";
+        document.getElementById('eventSpan').innerHTML = '<h2>dragEnd event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("zoom", function (params) {
+        document.getElementById('eventSpan').innerHTML = '<h2>zoom event:</h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("showPopup", function (params) {
+        document.getElementById('eventSpan').innerHTML = '<h2>showPopup event: </h2>' + JSON.stringify(params, null, 4);
+    });
+    network.on("hidePopup", function () {
+        console.log('hidePopup Event');
+    });
+    network.on("select", function (params) {
+        console.log('select Event:', params);
+    });
+    network.on("selectNode", function (params) {
+        console.log('selectNode Event:', params);
+    });
+    network.on("selectEdge", function (params) {
+        console.log('selectEdge Event:', params);
+    });
+    network.on("deselectNode", function (params) {
+        console.log('deselectNode Event:', params);
+    });
+    network.on("deselectEdge", function (params) {
+        console.log('deselectEdge Event:', params);
+    });
+    network.on("hoverNode", function (params) {
+        console.log('hoverNode Event:', params);
 };
-var options = {interaction:{hover:true}};
-var network = new vis.Network(container, data, options);
-
-network.on("click", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4);
-    console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
-});
-network.on("doubleClick", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>doubleClick event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("oncontext", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>oncontext (right click) event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("dragStart", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>dragStart event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("dragging", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>dragging event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("dragEnd", function (params) {
-    params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>dragEnd event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("zoom", function (params) {
-    document.getElementById('eventSpan').innerHTML = '<h2>zoom event:</h2>' + JSON.stringify(params, null, 4);
-});
-network.on("showPopup", function (params) {
-    document.getElementById('eventSpan').innerHTML = '<h2>showPopup event: </h2>' + JSON.stringify(params, null, 4);
-});
-network.on("hidePopup", function () {
-    console.log('hidePopup Event');
-});
-network.on("select", function (params) {
-    console.log('select Event:', params);
-});
-network.on("selectNode", function (params) {
-    console.log('selectNode Event:', params);
-});
-network.on("selectEdge", function (params) {
-    console.log('selectEdge Event:', params);
-});
-network.on("deselectNode", function (params) {
-    console.log('deselectNode Event:', params);
-});
-network.on("deselectEdge", function (params) {
-    console.log('deselectEdge Event:', params);
-});
-network.on("hoverNode", function (params) {
-    console.log('hoverNode Event:', params);
-});
-network.on("hoverEdge", function (params) {
-    console.log('hoverEdge Event:', params);
-});
-network.on("blurNode", function (params) {
-    console.log('blurNode Event:', params);
-});
-network.on("blurEdge", function (params) {
-    console.log('blurEdge Event:', params);
-});
+    });
+    network.on("hoverEdge", function (params) {
+        console.log('hoverEdge Event:', params);
+    });
+    network.on("blurNode", function (params) {
+        console.log('blurNode Event:', params);
+    });
+    network.on("blurEdge", function (params) {
+        console.log('blurEdge Event:', params);
+    });
 
