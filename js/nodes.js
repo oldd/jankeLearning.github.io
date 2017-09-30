34825b4e2e07de5da35799bbe0e1c9d587fafd75
colevandersWands <evan.t.cole@gmail.com>
2017-09-30 20:48:46 +0200

    vis interactions test

--- js/nodes.js
+++ js/nodes.js
@@ -1,3 +1,5 @@
 var nodes = [
-		{id:0, label:}
+		{id:0, title:<a href="https://github.com/jankeLearning/nodes/blob/master/sepco-projects-only/00-communications.md">communications</a>},
+		{id:1, title:<a href="https://github.com/jankeLearning/nodes/blob/master/sepco-projects-only/01-gh-portfolio.md">gh-portfolio</a>},
+		{id:2, title:<a href="https://github.com/jankeLearning/nodes/blob/master/sepco-projects-only/02-cleancalc-0-2.md">cleancalc 0-2</a>},
 	];
